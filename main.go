c158c788b9f5ddb32cdfb9fb8c1650e45a1334ae
David Johnson <dj@davids-mbp.lan>
2020-09-12 16:44:51 -0400

    .

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("sh","virsh list")
+	cmd := exec.Command("sh","-c", "sudo virt-install -n ubuntu-vm3 --connect qemu:///system --description Ubuntu --os-type=Linux --ram=1024 --vcpus=2 --disk path=/var/lib/libvirt/images/ubuntu-vm3.img,bus=virtio,size=4 --graphics spice --noautoconsole --location /home/jadmin/ubuntu14.iso --extra-args='console=tty0 console=ttyS0,115200n8 serial' --network bridge:br0")
 	stdoutStderr, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatal(err)
