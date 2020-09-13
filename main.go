81d9c84eb863ce7e94390611fb0cbf8db43319fc
David Johnson <dj@davids-mbp.lan>
2020-09-12 21:37:44 -0400

    .

--- main.go
+++ main.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
 	"log"
+	"net"
+	"time"
+	"github.com/digitalocean/go-libvirt"
 )
 
 func main() {
-	cmd := exec.Command("sh","-c", "sudo virt-install -n ubuntu-vm3 --connect qemu:///system --description Ubuntu --os-type=Linux --ram=1024 --vcpus=2 --disk path=/var/lib/libvirt/images/ubuntu-vm3.img,bus=virtio,size=4 --graphics spice --noautoconsole --location /home/jadmin/ubuntu14.iso --extra-args='console=tty0 console=ttyS0,115200n8 serial' --network bridge:br0")
-	stdoutStderr, err := cmd.CombinedOutput()
+// This dials libvirt on the local machine, but you can substitute the first
+	// two parameters with "tcp", "<ip address>:<port>" to connect to libvirt on
+	// a remote machine.
+	c, err := net.DialTimeout("unix", "/var/run/libvirt/libvirt-sock", 2*time.Second)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to dial libvirt: %v", err)
 	}
-	fmt.Printf("%s\n", stdoutStderr)
+
+	l := libvirt.New(c)
+	if err := l.Connect(); err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+
+	v, err := l.Version()
+	if err != nil {
+		log.Fatalf("failed to retrieve libvirt version: %v", err)
+	}
+	fmt.Println("Version:", v)
+
+	domains, err := l.Domains()
+	if err != nil {
+		log.Fatalf("failed to retrieve domains: %v", err)
+	}
+
+	fmt.Println("ID\tName\t\tUUID")
+	fmt.Printf("--------------------------------------------------------\n")
+	for _, d := range domains {
+		fmt.Printf("%d\t%s\t%x\n", d.ID, d.Name, d.UUID)
+	}
+
+	if err := l.Disconnect(); err != nil {
+		log.Fatalf("failed to disconnect: %v", err)
+	}
 }
